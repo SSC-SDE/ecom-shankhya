92b6be72e635610bfafd943d8612d3961ed51bc9
Shankhya Subhra Chatterjee <shankhyasubhrac@outlook.com>
2025-03-18 00:30:45 +0530

    Coupoon Routes and Controller

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 console.log("PortEnvioronment", PORT);
 
@@ -31,6 +33,3 @@ app.listen(PORT, () => {
     connectDB();
 
 })
-
-//sSPhqpo38PLpELFS
-//mongo pass
