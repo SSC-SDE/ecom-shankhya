e92ea8560f87c97fb6a113ec845381b9b866d585
Shankhya Subhra Chatterjee <shankhyasubhrac@outlook.com>
2025-03-18 00:17:04 +0530

    Cart Setup

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 //Routes
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 console.log("PortEnvioronment", PORT);
 
