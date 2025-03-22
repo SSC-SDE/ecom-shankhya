8fc7c66eee22260382a6a6f0e1f9fe48a7338c60
Shankhya Subhra Chatterjee <shankhyasubhrac@outlook.com>
2025-03-23 03:07:08 +0530

    Render Deployment Stuff

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 //Routes
 import authRoutes from "./routes/auth.route.js";
@@ -19,6 +20,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve()
+
 app.use(express.json({ limit: "10mb" })); //allows you to parse the body of the request
 app.use(cookieParser());
 
@@ -30,6 +33,13 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 console.log("PortEnvioronment", PORT);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log("The port is running on http://localhost:" + PORT);
